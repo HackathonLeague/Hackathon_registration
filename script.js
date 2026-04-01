7ada4a0982252adf96fe308f8d400130a367379f
Fateless1 <baymyrzaevaruubek10@gmail.com>
2026-04-01 18:55:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,40 @@ function initCarousel() {
   const total = imgs.length / 2
   let index = 0
 
+  let intervalId = null
+  let resumeTimeout = null
+
   function update(animate = true) {
     const width = imgs[0].offsetWidth
-
     track.style.transition = animate ? 'transform 0.2s ease' : 'none'
     track.style.transform = `translateX(-${index * width}px)`
   }
 
+  function startAuto() {
+    intervalId = setInterval(() => {
+      index++
+      update()
+
+      if (index === total) {
+        setTimeout(() => {
+          index = 0
+          update(false)
+        }, 200)
+      }
+    }, 1200)
+  }
+
+  function pauseAuto() {
+    clearInterval(intervalId)
+    clearTimeout(resumeTimeout)
+
+    resumeTimeout = setTimeout(() => {
+      startAuto()
+    }, 2000)
+  }
+
   document.getElementById('next').onclick = () => {
+    pauseAuto()
     index++
     update()
 
@@ -25,6 +51,8 @@ function initCarousel() {
   }
 
   document.getElementById('prev').onclick = () => {
+    pauseAuto()
+
     if (index === 0) {
       index = total
       update(false)
@@ -36,17 +64,7 @@ function initCarousel() {
     }, 0)
   }
 
-  setInterval(() => {
-    index++
-    update()
-
-    if (index === total) {
-      setTimeout(() => {
-        index = 0
-        update(false)
-      }, 200)
-    }
-  }, 1200)
+  startAuto()
 }
 
 window.onload = initCarousel
